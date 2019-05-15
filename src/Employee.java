6a5237f15fbe4c46492ece0d2b26c0dd8c1212cc
Simon Blaha <s.blaha@live.at>
2019-05-15 10:32:04 +0200

    create get methode for name

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,22 @@
 public class Employee {
+
+    private String name;
+    private String surname;
+    private int yearOfBirth;
+    private double salary;
+
+    public Employee(String name, String surname, int yearOfBirth, double salary) {
+        this.name = name;
+        this.surname = surname;
+        this.yearOfBirth = yearOfBirth;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getSurname() {
+        return this.surname;
+    }
 }
